a4f2a0e1f25fbb67a8e5f3628e7e5f382d22c785
Mizpuno <nititorn.certihunt@gmail.com>
2023-09-09 14:53:54 +0700

    update some html

--- webpost-domain-model/src/main/java/lab/webpost/domain/Post.java
+++ webpost-domain-model/src/main/java/lab/webpost/domain/Post.java
@@ -8,6 +8,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
@@ -30,6 +31,7 @@ public class Post {
     private LocalDateTime createdAt;
 
     @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "author_id", referencedColumnName = "id")
     private User author;
 
     
